d7ee7127c5ee5f5a0f162635e10b0fc9e250b6ef
Rami Rahikkala <rami.rahikkala@jotautomation.com>
2020-11-14 10:35:19 +0200

    Initial commit
    Signed-off-by: Rami Rahikkala <rami.rahikkala@jotautomation.com>

---
+++ pi_environment_logger.py
@@ -0,0 +1,36 @@
+import time
+import os
+from pymongo import MongoClient
+import Adafruit_BMP.BMP085 as BMP085
+from datetime import datetime
+
+FREQUENCY_SECONDS = 30
+
+
+def main():
+    client = MongoClient(
+        'mongodb+srv://'
+        + os.environ['KOTI_CONNECTION_USER']
+        + ':'
+        + os.environ['KOTI_CONNECTION_PWD']
+        + '@'
+        + os.environ['KOTI_CONNECTION']
+        + '/?retryWrites=true&w=majority'
+    )
+
+    db = client.environment
+    bmp = BMP085.BMP085()
+
+    previous = None
+
+    while True:
+        environment = {
+            'temp': bmp.read_temperature(),
+            'pressure': bmp.read_pressure(),
+            'altitude': bmp.read_altitude(),
+            'date': datetime.now(),
+            'location': os.environ['KOTI_LOCATION']}
+        if environment != previous:
+            db.reviews.insert_one(environment)
+            previous = environment
+        time.sleep(FREQUENCY_SECONDS)
